358dbb246989f1635e8c1ba2de070520a233b301
Noah Baldinger <baldingernoah@proton.me>
2024-09-17 08:01:53 +0200

    adding new laptop dir

---
+++ Laptop/dotfiles/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar top1 -c ~/.config/polybar/config.ini &
+polybar top2 -c ~/.config/polybar/config.ini &
